8e843c0a250ee83409d7e2acca9032c36d5c6e85
Emmanuel Sasere <emmanuelscript.code@gmail.com>
2023-06-03 14:34:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// MENU TOGGLE
+const myMenu = document.querySelector('.menu-toggle');
+const myList = document.querySelector('.nav__menu');
+
+myMenu.addEventListener('click', () => {
+    myMenu.classList.toggle('show');
+    myList.classList.toggle('show');
+});
+
+// CLOSE THE NAV WHEN NAVLNKS ARE CLICKED
+let navLinks = document.querySelectorAll('.nav__menu a');
+
+navLinks.forEach(function (navLink) {
+    navLink.addEventListener('click', function () {
+        myList.classList.remove('show');
+    })
+})
+
+
+
+// SWIPER JS CODE ==================
+const swiper = new Swiper('.swiper', {
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
